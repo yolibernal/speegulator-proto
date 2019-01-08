d689d3ceb9cee0dc45910023c28f38898cc2643d
Jonas Bernhard <jonasbernhard96@gmail.com>
2019-01-09 00:03:07 +0100

    Increase min sdk version and install react native ble plx

--- android/app/src/main/java/com/speegulatorproto/MainApplication.java
+++ android/app/src/main/java/com/speegulatorproto/MainApplication.java
@@ -3,6 +3,7 @@ package com.speegulatorproto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlePackage(),
             new VectorIconsPackage(),
             new TextToSpeechPackage(),
             new RNGestureHandlerPackage()
