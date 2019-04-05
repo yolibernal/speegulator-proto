43458fc4c266c73c3e2680f972ff9e2eccf1812e
Jonas Bernhard <jonasbernhard96@gmail.com>
2019-04-05 13:34:30 +0200

    Install Mapbox React Native SDK for android

--- android/app/src/main/java/com/speegulatorproto/MainApplication.java
+++ android/app/src/main/java/com/speegulatorproto/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.mapbox.rctmgl.RCTMGLPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
             new BlePackage(),
             new VectorIconsPackage(),
             new TextToSpeechPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new RCTMGLPackage()
       );
     }
 
