9b93c1204700f5494819bdd6496f40701f04e6af
Jonas Bernhard <jonasbernhard96@gmail.com>
2019-04-06 21:13:19 +0200

    Add event handler and clean up styles

--- src/scenes/RouteMap/index.tsx
+++ src/scenes/RouteMap/index.tsx
@@ -1,19 +1,27 @@
 import { connect } from 'react-redux'
 import React from 'react'
 import MapboxGL from '@mapbox/react-native-mapbox-gl'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
+import styles from './styles'
+/*
+event example
+{
+  properties: { screenPointY: 501.328125, screenPointX: 333.28125 },
+  geometry:
+  {
+    coordinates: [-122.09748154853304, 37.43706374560975],
+      type: 'Point'
+  },
+  type: 'Feature'
+}
+*/
 
 type Props = {
 }
 
 type ComponentState = {
+  selectedWaypoints: number[][]
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 class RouteMap extends React.Component<Props, ComponentState> {
   static navigationOptions = {
@@ -22,6 +30,9 @@ class RouteMap extends React.Component<Props, ComponentState> {
 
   constructor(props) {
     super(props)
+    this.state = {
+      selectedWaypoints: []
+    }
   }
 
   render() {
@@ -32,10 +43,24 @@ class RouteMap extends React.Component<Props, ComponentState> {
           zoomLevel={12}
           userTrackingMode={MapboxGL.UserTrackingModes.Follow}
           styleURL={MapboxGL.StyleURL.Street}
-          style={styles.container}
+          style={styles.map}
+          onPress={event => this.handleMapPress(event)}
         />
+        <View style={styles.footer}>
+          <Text>
+            Selected waypoints:
+            {this.state.selectedWaypoints}
+          </Text>
+        </View>
       </View>
     )
+  }
+
+  handleMapPress(event) {
+    const { coordinates: waypointCoordinates } = event.geometry
+    this.setState(state => ({
+      selectedWaypoints: [...state.selectedWaypoints, waypointCoordinates]
+    }))
   }
 }
 
--- src/scenes/RouteMap/styles.ts
+++ src/scenes/RouteMap/styles.ts
@@ -1,5 +1,14 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  map: {
+    flex: 7
+  },
+  footer: {
+    flex: 1
+  }
 })
